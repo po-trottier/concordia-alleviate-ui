48179e79df608a70ba08630d603ca57fe4737bd6
Pierre-Olivier Trottier <po.trottier@gmail.com>
2021-04-15 01:34:46 -0400

    Added most of the journal layout

--- mobile/src/main/java/com/concordia/alleviate/ui/journal/JournalFragment.java
+++ mobile/src/main/java/com/concordia/alleviate/ui/journal/JournalFragment.java
@@ -8,9 +8,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.concordia.alleviate.R;
@@ -19,42 +17,65 @@ import pl.pawelkleczkowski.customgauge.CustomGauge;
 public class JournalFragment extends Fragment {
 
     private final int ANIMATION_TIME = 5;
-    private final int PROGRESS_MINIMUM = 10;
-    private final int PROGRESS_MAXIMUM = 95;
 
     private Activity activity;
     private JournalViewModel vm;
+
+    private CustomGauge stressGauge;
+    private TextView stressView;
+    private TextView alleviatedView;
+    private TextView heartRateView;
+    private TextView bloodPressureTopView;
+    private TextView bloodPressureBottomView;
+    private TextView agitationView;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         activity = getActivity();
         vm = new ViewModelProvider(this).get(JournalViewModel.class);
 
         View root = inflater.inflate(R.layout.fragment_journal, container, false);
+        findViews(root);
 
-        int stressValue = (int) (Math.random() * (PROGRESS_MAXIMUM - PROGRESS_MINIMUM + 1) + PROGRESS_MINIMUM);
+        vm.generateRandomData();
+        updateViewsData();
 
-        CustomGauge gauge = root.findViewById(R.id.journal_progress_bar);
+        return root;
+    }
+
+    private void findViews(View root) {
+        stressGauge = root.findViewById(R.id.journal_progress_bar);
+        stressView = root.findViewById(R.id.journal_stress_text);
+        alleviatedView = root.findViewById(R.id.journal_alleviated_text);
+        heartRateView = root.findViewById(R.id.journal_heart_text);
+        bloodPressureTopView = root.findViewById(R.id.journal_pressure_top_text);
+        bloodPressureBottomView = root.findViewById(R.id.journal_pressure_bottom_text);
+        agitationView = root.findViewById(R.id.journal_agitation_text);
+    }
+
+    private void updateViewsData() {
+        vm.getAlleviatedTime().observe(getViewLifecycleOwner(), (i) -> alleviatedView.setText(Integer.toString(i)));
+        vm.getHeartRate().observe(getViewLifecycleOwner(), (i) -> heartRateView.setText(Integer.toString(i)));
+        vm.getBloodPressureTop().observe(getViewLifecycleOwner(), (i) -> bloodPressureTopView.setText(Integer.toString(i)));
+        vm.getBloodPressureBottom().observe(getViewLifecycleOwner(), (i) -> bloodPressureBottomView.setText("/ " + i));
+        vm.getAgitationLevel().observe(getViewLifecycleOwner(), (i) -> agitationView.setText(Integer.toString(i)));
+        vm.getStressLevel().observe(getViewLifecycleOwner(), (i) -> {
+            stressView.setText(Integer.toString(i));
+            updateProgressBar(i);
+        });
+    }
+
+    private void updateProgressBar(int stressValue) {
         new Thread(() -> {
             try {
                 Thread.sleep(600);
                 for (int i = 0 ; i < 100 ; i++) {
                     float percentage = (float) (i + 1) / 100;
-                    activity.runOnUiThread(() -> gauge.setValue((int) (stressValue * percentage)));
+                    activity.runOnUiThread(() -> stressGauge.setValue((int) (stressValue * percentage)));
                     Thread.sleep((long) ((float) (ANIMATION_TIME * stressValue) / 100));
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }).start();
-
-        final TextView textView = root.findViewById(R.id.text_home);
-        vm.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-
-        return root;
     }
 }
--- mobile/src/main/java/com/concordia/alleviate/ui/journal/JournalViewModel.java
+++ mobile/src/main/java/com/concordia/alleviate/ui/journal/JournalViewModel.java
@@ -1,19 +1,104 @@
 package com.concordia.alleviate.ui.journal;
 
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class JournalViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private final int STRESS_MINIMUM = 10;
+    private final int STRESS_MAXIMUM = 95;
+    private final int ALEVIATED_MINIMUM = 1;
+    private final int ALEVIATED_MAXIMUM = 14;
+    private final int HEART_MINIMUM = 60;
+    private final int HEART_MAXIMUM = 120;
+    private final int PRESSURE_TOP_MINIMUM = 110;
+    private final int PRESSURE_TOP_MAXIMUM = 130;
+    private final int PRESSURE_BOTTOM_MINIMUM = 60;
+    private final int PRESSURE_BOTTOM_MAXIMUM = 90;
+    private final int AGITATION_MINIMUM = 1;
+    private final int AGITATION_MAXIMUM = 99;
+
+    private final MutableLiveData<Integer> stressLevel;
+    private final MutableLiveData<Integer> alleviatedTime;
+    private final MutableLiveData<Integer> heartRate;
+    private final MutableLiveData<Integer> bloodPressureTop;
+    private final MutableLiveData<Integer> bloodPressureBottom;
+    private final MutableLiveData<Integer> agitationLevel;
 
     public JournalViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        stressLevel = new MutableLiveData<>();
+        stressLevel.setValue(0);
+
+        alleviatedTime = new MutableLiveData<>();
+        alleviatedTime.setValue(0);
+
+        heartRate = new MutableLiveData<>();
+        heartRate.setValue(0);
+
+        bloodPressureTop = new MutableLiveData<>();
+        bloodPressureTop.setValue(0);
+
+        bloodPressureBottom = new MutableLiveData<>();
+        bloodPressureBottom.setValue(0);
+
+        agitationLevel = new MutableLiveData<>();
+        agitationLevel.setValue(0);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public void generateRandomData() {
+        setStressLevel((int) (Math.random() * (STRESS_MAXIMUM - STRESS_MINIMUM + 1) + STRESS_MINIMUM));
+        setAlleviatedTime((int) (Math.random() * (ALEVIATED_MAXIMUM - ALEVIATED_MINIMUM + 1) + ALEVIATED_MINIMUM));
+        setHeartRate((int) (Math.random() * (HEART_MAXIMUM - HEART_MINIMUM + 1) + HEART_MINIMUM));
+        setBloodPressureTop((int) (Math.random() * (PRESSURE_TOP_MAXIMUM - PRESSURE_TOP_MINIMUM + 1) + PRESSURE_TOP_MINIMUM));
+        setBloodPressureBottom((int) (Math.random() * (PRESSURE_BOTTOM_MAXIMUM - PRESSURE_BOTTOM_MINIMUM + 1) + PRESSURE_BOTTOM_MINIMUM));
+        setAgitationLevel((int) (Math.random() * (AGITATION_MAXIMUM - AGITATION_MINIMUM + 1) + AGITATION_MINIMUM));
+    }
+
+    public MutableLiveData<Integer> getStressLevel() {
+        return stressLevel;
+    }
+
+    public void setStressLevel(int stressLevel) {
+        this.stressLevel.setValue(stressLevel);
+    }
+
+    public MutableLiveData<Integer> getAlleviatedTime() {
+        return alleviatedTime;
+    }
+
+    public void setAlleviatedTime(int alleviatedTime) {
+        this.alleviatedTime.setValue(alleviatedTime);
+    }
+
+    public MutableLiveData<Integer> getHeartRate() {
+        return heartRate;
+    }
+
+    public void setHeartRate(int heartRate) {
+        this.heartRate.setValue(heartRate);
+    }
+
+    public MutableLiveData<Integer> getBloodPressureTop() {
+        return bloodPressureTop;
+    }
+
+    public void setBloodPressureTop(int bloodPressureTop) {
+        this.bloodPressureTop.setValue(bloodPressureTop);
+    }
+
+    public MutableLiveData<Integer> getBloodPressureBottom() {
+        return bloodPressureBottom;
+    }
+
+    public void setBloodPressureBottom(int bloodPressureBottom) {
+        this.bloodPressureBottom.setValue(bloodPressureBottom);
+    }
+
+    public MutableLiveData<Integer> getAgitationLevel() {
+        return agitationLevel;
+    }
+
+    public void setAgitationLevel(int agitationLevel) {
+        this.agitationLevel.setValue(agitationLevel);
     }
 }
