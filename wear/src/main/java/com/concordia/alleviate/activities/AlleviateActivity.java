66d10cc8dfa54f31fa9418ad893222252dd04639
Pierre-Olivier Trottier <po.trottier@gmail.com>
2021-04-23 20:46:06 -0400

    Started Alleviate Layout

--- wear/src/main/java/com/concordia/alleviate/activities/AlleviateActivity.java
+++ wear/src/main/java/com/concordia/alleviate/activities/AlleviateActivity.java
@@ -5,31 +5,46 @@ import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
 import com.concordia.alleviate.R;
+import pl.pawelkleczkowski.customgauge.CustomGauge;
 
 public class AlleviateActivity extends WearableActivity {
+
+    private static final String DEFAULT_EXERCISE = "Lion’s breath";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alleviate);
         setIntentExercise();
+        startExercise();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         setIntentExercise();
+        startExercise();
     }
 
     private void setIntentExercise() {
-        // TODO Change this to fit the design
+        // Set default value
+        TextView exerciseView = findViewById(R.id.alleviate_text);
+        exerciseView.setText(DEFAULT_EXERCISE);
+        // Override default value
         Intent intent = getIntent();
         if (intent != null && intent.getExtras() != null) {
             String selectedExercise = intent.getExtras().getString("exercise");
             if (selectedExercise != null && selectedExercise.length() > 0) {
-                TextView demoTextView = findViewById(R.id.alleviate_text);
-                demoTextView.setText(selectedExercise);
+                exerciseView.setText(selectedExercise);
             }
         }
+    }
+
+    private void startExercise() {
+        CustomGauge exerciseGauge = findViewById(R.id.alleviate_progress_bar);
+        TextView actionView = findViewById(R.id.alleviate_text_action);
+
+        exerciseGauge.setValue(50);
+        actionView.setText("Breath In");
     }
 }
